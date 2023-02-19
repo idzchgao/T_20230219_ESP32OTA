72e2ebcafd82dbad58dc23f50a1be9dfd0f076e7
Stonebraker <zchgao@gmail.com>
2023-02-19 23:15:28 +0800

    v2.3
    updates

--- 20230219_ESP32OTA/20230219_ESP32OTA.ino
+++ 20230219_ESP32OTA/20230219_ESP32OTA.ino
@@ -1,6 +1,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <HTTPUpdate.h>
+#include <ESP32httpUpdate.h>
 
 const char * ssid = "JOEGHOME";
 const char * password = "sll32401";
@@ -8,10 +8,10 @@ const char * password = "sll32401";
 WiFiClient client;
 HTTPClient https;
 
-String FirmwareVer = "2.2";
+String FirmwareVer = "2.3";
 
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/idzchgao/T_20230219_ESP32OTA/main/ver.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/idzchgao/T_20230219_ESP32OTA/main/fw.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
@@ -21,14 +21,15 @@ void firmwareUpdate();
 int FirmwareVersionCheck();
 
 unsigned long previousMillis = 0; // will store last time LED was updated
-const long interval = 10000;
+const long interval = 60000;
 
 void repeatedCall() {
   unsigned long currentMillis = millis();
   if ((currentMillis - previousMillis) >= interval) 
   {
     previousMillis = currentMillis;
-    if (FirmwareVersionCheck()) 
+    
+    if(FirmwareVersionCheck())    //if (FirmwareVersionCheck()) 
     {
       firmwareUpdate();
     }
@@ -64,35 +65,70 @@ void connect_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+void update_started() {
+  Serial.println("CALLBACK:  HTTP update process started");
+}
+
+void update_finished() {
+  Serial.println("CALLBACK:  HTTP update process finished");
+}
+
+void update_progress(int cur, int total) {
+  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+
+void update_error(int err) {
+  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+
 void firmwareUpdate(void) {
 //  httpUpdate.setLedPin(LED_BUILTIN, LOW);
-  t_httpUpdate_return ret = httpUpdate.update(client, URL_fw_Bin);
-
-  switch (ret) {
-  case HTTP_UPDATE_FAILED:
-    Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
-    break;
-
-  case HTTP_UPDATE_NO_UPDATES:
-    Serial.println("HTTP_UPDATE_NO_UPDATES");
-    break;
-
-  case HTTP_UPDATE_OK:
-    Serial.println("HTTP_UPDATE_OK");
-    break;
+  if ((WiFi.status() == WL_CONNECTED))
+  {    
+//    WiFiClient client;
+    // The line below is optional. It can be used to blink the LED on the board during flashing
+    // The LED will be on during download of one buffer of data from the network. The LED will
+    // be off during writing that buffer to flash
+    // On a good connection the LED should flash regularly. On a bad connection the LED will be
+    // on much longer than it will be off. Other pins than LED_BUILTIN may be used. The second
+    // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
+    // httpUpdate.setLedPin(LED_BUILTIN, LOW);
+//
+//    ESPhttpUpdate.onStart(update_started);
+//    ESPhttpUpdate.onEnd(update_finished);
+//    ESPhttpUpdate.onProgress(update_progress);
+//    ESPhttpUpdate.onError(update_error);
+  
+    t_httpUpdate_return ret = ESPhttpUpdate.update(URL_fw_Bin);
+  
+    switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+  
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
+  
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      break;
+    }    
+    
   }
+
 }
 int FirmwareVersionCheck(void) {
   String payload;
   int httpCode;
-  String fwurl = "";
-  fwurl += URL_fw_Version;
-  fwurl += "?";
-  fwurl += String(rand());
-  Serial.println(fwurl);
+//  String fwurl = "";
+//  fwurl += URL_fw_Version;
+//  fwurl += "?";
+//  fwurl += String(rand());
+//  Serial.println(fwurl);
 //  WiFiClientSecure * client = new WiFiClientSecure;
   
-  if (https.begin(fwurl)) 
+  if (https.begin(URL_fw_Version)) 
   { // HTTPS      
     Serial.print("[HTTPS] GET...\n");
     // start connection and send HTTP header
@@ -113,7 +149,7 @@ int FirmwareVersionCheck(void) {
   {
     payload.trim();
     if (payload.equals(FirmwareVer)) {
-      Serial.printf("\nDevice already on latest firmware version:%s\n", FirmwareVer);
+      Serial.printf("\r\nDevice already on latest firmware version:%s\r\n", FirmwareVer);
       return 0;
     } 
     else 
